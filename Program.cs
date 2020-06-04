ae0e51ac3e0b3ce4545c5074f6c355704fcb39b1
petergloor <p.gloor@notesdev.ch>
2020-06-04 14:34:37 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace hallo
+{
+    class Program
+    {
+        private const string Value = "Hallo V1.0";
+
+        static void Main(string[] args) => Console.WriteLine(value: Value);
+    }
+}
