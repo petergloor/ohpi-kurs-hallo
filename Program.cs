b13765a166b5ccd85b64621d80367c46d6135097
petergloor <p.gloor@notesdev.ch>
2020-06-04 14:37:58 +0200

    Update V2.0

--- Program.cs
+++ Program.cs
@@ -4,8 +4,7 @@ namespace hallo
 {
     class Program
     {
-        private const string Value = "Hallo V1.0";
-
+        private const string Value = "Hallo V2.0";
         static void Main(string[] args) => Console.WriteLine(value: Value);
     }
 }
